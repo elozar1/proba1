9052303626355b735fd2feb16dfb05b133decdcf
elozar1 <edilozar1@gmail.com>
2023-05-14 15:59:38 +0200

    javascript update

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ const zaposlenik_btn = document.querySelector("#zaposlenik");
 const manager_btn = document.querySelector("#manager");
 const name_input = document.querySelector("#name-input");
 
+const path =
+  "C:\\Users\\Edi\\Downloads\\Evidencija studenata (2020-11-19).xlsx";
+
 const getZaposlenik = () => {
   const name = name_input.value;
-  console.log(name);
+  const reader = new FileReader();
+  reader.onload = function () {
+    reader.result;
+  };
+  reader.readAsText();
 };
-
+const text = new Blob();
 zaposlenik_btn.addEventListener("click", getZaposlenik);
